a1e34713b7192e744acd21c02c897f852b300d61
Fran Gregori Tandazo <fran@MacBook-Pro-de-Fran-2.local>
2023-05-25 15:20:08 +0200

    Estructura realizada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module es.progcipfpbatoi {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens es.progcipfpbatoi to javafx.fxml;
+    opens es.progcipfpbatoi.controlador to javafx.fxml;
     exports es.progcipfpbatoi;
 }
