fc018a8cb9b0f6cbe36594e54b3fb9ea0fa437c3
rogeret03 <mollseguiroger@gmail.com>
2023-05-25 17:05:59 +0200

    temporada DAO getFromResultSet

--- src/main/java/es/progcipfpbatoi/modelo/dao/TemporadaDAO.java
+++ src/main/java/es/progcipfpbatoi/modelo/dao/TemporadaDAO.java
@@ -1,8 +1,13 @@
 package es.progcipfpbatoi.modelo.dao;
 
 import es.progcipfpbatoi.exceptions.DatabaseErrorException;
+import es.progcipfpbatoi.modelo.dto.Produccion;
 import es.progcipfpbatoi.modelo.dto.Temporada;
 
+import java.util.ArrayList;
+
 public interface TemporadaDAO {
+
+    ArrayList<Temporada> findAll() throws DatabaseErrorException;
     Temporada findById(int id)  throws DatabaseErrorException;
 }
