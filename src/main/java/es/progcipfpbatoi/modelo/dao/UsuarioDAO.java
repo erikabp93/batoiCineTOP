4aa9f34d6a50879d6db9ccee5fae1f6d9695681c
erikabp93 <eribalpas@alu.edu.gva.es>
2023-05-26 17:18:33 +0200

    Login y registro hecho

--- src/main/java/es/progcipfpbatoi/modelo/dao/UsuarioDAO.java
+++ src/main/java/es/progcipfpbatoi/modelo/dao/UsuarioDAO.java
@@ -6,4 +6,5 @@ import es.progcipfpbatoi.modelo.dto.Usuario;
 public interface UsuarioDAO {
 
     Usuario save(Usuario usuario) throws DatabaseErrorException;
+    boolean existeUsuario(Usuario usuario) throws DatabaseErrorException;
 }
