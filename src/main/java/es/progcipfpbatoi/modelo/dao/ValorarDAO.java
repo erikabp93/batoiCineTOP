0b799f265297290b423fbf26a2cdfe08e75a0e7a
rogeret03 <mollseguiroger@gmail.com>
2023-05-26 16:03:20 +0200

    ValorarDAO

---
+++ src/main/java/es/progcipfpbatoi/modelo/dao/ValorarDAO.java
@@ -0,0 +1,14 @@
+package es.progcipfpbatoi.modelo.dao;
+
+import es.progcipfpbatoi.exceptions.DatabaseErrorException;
+import es.progcipfpbatoi.modelo.dto.Produccion;
+import es.progcipfpbatoi.modelo.dto.Usuario;
+
+import java.util.ArrayList;
+
+public interface ValorarDAO {
+
+    ArrayList<Produccion> findAll(Produccion produccion) throws DatabaseErrorException;
+    boolean save(Produccion produccion, Usuario usuario, int valoracion, String comentario) throws DatabaseErrorException;
+
+}
