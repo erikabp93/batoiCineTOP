0237e1c8f1a357d1f50f442c664f3a5b88fa0e5e
erikabp93 <eribalpas@alu.edu.gva.es>
2023-05-30 20:51:18 +0200

    Alertas login hechas

--- src/main/java/es/progcipfpbatoi/util/DatosBD.java
+++ src/main/java/es/progcipfpbatoi/util/DatosBD.java
@@ -2,7 +2,7 @@ package es.progcipfpbatoi.util;
 
 public class DatosBD {
 
-    public static final String IP = "172.16.226.93";
+    public static final String IP = "172.16.226.96";
     public static final String DATABASE = "batoiCine_bd";
     public static final String USERNAME = "admin";
     public static final String PASSWORD = "1234";
