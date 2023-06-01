3af1fb7212eb5b2cac3c574bd84c27362bab843e
rogeret03 <mollseguiroger@gmail.com>
2023-06-01 17:14:04 +0200

    vista busqueda

--- src/main/java/es/progcipfpbatoi/util/DatosBD.java
+++ src/main/java/es/progcipfpbatoi/util/DatosBD.java
@@ -2,9 +2,9 @@ package es.progcipfpbatoi.util;
 
 public class DatosBD {
 
-    public static final String IP = "172.16.226.96";
+    public static final String IP = "172.16.226.123";
     public static final String DATABASE = "batoiCine_bd";
-    public static final String USERNAME = "admin";
+    public static final String USERNAME = "batoi";
     public static final String PASSWORD = "1234";
 
 }
