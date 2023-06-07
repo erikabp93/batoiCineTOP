7fab03739de2efc2885c1762f66be2c62dd4282c
erikabp93 <eribalpas@alu.edu.gva.es>
2023-06-07 23:27:53 +0200

    JavaDoc de carpeta Validator

--- src/main/java/es/progcipfpbatoi/Validator/Validador.java
+++ src/main/java/es/progcipfpbatoi/Validator/Validador.java
@@ -4,17 +4,47 @@ public class Validador {
 
     private Validator validator;
 
+    /**
+     * Constructor de la clase, obligatorio ponerlo, ya que si no se pone nos da warning
+     */
     public Validador() {
         this.validator = new Validator();
     }
+
+    /**
+     * Esta clase sirve para validar la contraseña, lo que se pasa como parámetro se le pasa
+     * al método que está en la clase Validator, esta devuelve true o false dependiendo que
+     * lo que el método le devuelva
+     *
+     * @param contrasenya
+     * @return
+     */
 
     public boolean validarContrasenya(String contrasenya) {
         return validator.validarContrasenya(contrasenya);
     }
 
+    /**
+     * Esta clase sirve para validar el nombre del usuario, lo que se pasa como parámetro se
+     * le pasa al método que está en la clase Validator, esta devuelve true o false
+     * dependiendo que lo que el método le devuelva
+     *
+     * @param nombreUsuario
+     * @return
+     */
+
     public boolean nombreUsuarioCorrecto(String nombreUsuario) {
         return validator.nombreUsuarioCorrecto(nombreUsuario);
     }
+
+    /**
+     * Esta clase sirve para validar el correo, lo que se pasa como parámetro se le pasa
+     * al método que está en la clase Validator, esta devuelve true o false dependiendo
+     * que lo que el método le devuelva
+     *
+     * @param correo
+     * @return boolean
+     */
 
     public boolean correoCorrecto(String correo) {
         return validator.correoCorrecto(correo);
