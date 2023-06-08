452a8e15d025ecf0b13148b4e8864513fd6fd47e
rogeret03 <mollseguiroger@gmail.com>
2023-06-08 20:34:35 +0200

    test filtrarGenero

--- src/main/java/es/progcipfpbatoi/util/DatosBD.java
+++ src/main/java/es/progcipfpbatoi/util/DatosBD.java
@@ -2,7 +2,7 @@ package es.progcipfpbatoi.util;
 
 public class DatosBD {
 
-    public static final String IP = "172.16.225.142";
+    public static final String IP = "172.16.226.56";
     public static final String DATABASE = "batoiCine_bd";
     public static final String USERNAME = "admin";
     public static final String PASSWORD = "1234";
